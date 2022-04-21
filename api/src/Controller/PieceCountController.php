168aee2aee41e2d2d63ee07a4834f72d2089cec0
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-04-21 09:40:03 +0200

    Ajout de documentation dans le scope de la fonction

--- api/src/Controller/PieceCountController.php
+++ api/src/Controller/PieceCountController.php
@@ -7,11 +7,12 @@ use App\Entity\Piece;
 class PieceCountController
 {
     /**
-     * @param Piece $data Ici $data représente la pièce d'id passer en paramètre de l'URL pieces/{id}/countNbPers
-     * @return int La valeur du champ nbPers de la table Piece 'caster' en int
+     * @param Piece $data Ici $data représente la pièce d'id passer en paramètre de l'URL pieces/{id}/countNbPers.
+     * @return int La valeur du champ nbPers de la table Piece 'caster' en int.
      */
     public function __invoke(Piece $data): int
     {
-          return $data->getNbPers();
+        // On retourne le nombre de champs nbPers de la table pièce
+        return $data->getNbPers();
     }
 }
