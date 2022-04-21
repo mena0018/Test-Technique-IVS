bc1e07c3c84f2a30eb0cb867827d39e0cea836e3
Menad <rabie.menad@etudiant.univ-reims.fr>
2022-04-21 14:52:07 +0200

    Ajout de documentation + PropTypes

--- front/src/Components/Building/BuildingList.js
+++ front/src/Components/Building/BuildingList.js
@@ -1,7 +1,10 @@
 import BuildingItem from "./BuildingItem"
+import PropTypes from "prop-types";
 
 
-export default function Building({ datas }) {
+export default function BuildingList({ datas }) {
+
+   // On récupère la liste des buildings que l'on parcours afin de renvoyer les données d'un building par composant. On donne un identifiant pour la props key pour que React s'y retrouve.
 
    const listBuildings = datas.map((building) => (
       <BuildingItem  datas={building} key={building.id}/>
@@ -9,7 +12,14 @@ export default function Building({ datas }) {
    
   return (
     <div className='building-list'>
+       {/* listBuildings correspond à une liste de composant BuildingItem */}
        {listBuildings}
     </div>
   )
+}
+
+// Ajout de PropTypes afin de vérifier les données que l'on reçoit.
+// Ici, la props datas est un tableau de buildings
+BuildingList.propTypes  = {
+   datas: PropTypes.array
 }
